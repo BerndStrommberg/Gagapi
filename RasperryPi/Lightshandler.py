20d213401da59dc0d162b2e1943eae6f81d666ab
MoritzStoll <moe-stoll@web.de>
2018-09-21 11:00:52 +0200

    schneeell

--- RasperryPi/Lightshandler.py
+++ RasperryPi/Lightshandler.py
@@ -1,14 +1,16 @@
 import LightshandlerTools as lgt
-
-
+import time
+import json
 def run():
     while (True):
+        response = str(lgt.get_current_state("192.168.0.97","8080"))
+        print(response)
+        act_json = json.loads(response)
+        print(act_json['activity_num'])
+        lgt.change_light(act_json['activity_num'])
+        time.sleep(10)
         # Erst State vom Server Holen
         # Dann Hue anpassen
-        
-    
 
-
-
-if name == "__main__":
+if __name__ == "__main__":
     run()
